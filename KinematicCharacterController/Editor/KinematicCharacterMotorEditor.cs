e1c5b198be3f785a68be535a035652135f5e8943
林鑫 <linxin025@gmail.com>
2025-04-09 10:59:49 +0800

    Add kinematic character controller files

---
+++ KinematicCharacterController/Editor/KinematicCharacterMotorEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace KinematicCharacterController
+{
+    [CustomEditor(typeof(KinematicCharacterMotor))]
+    public class KinematicCharacterMotorEditor : Editor
+    {
+        protected virtual void OnSceneGUI()
+        {
+            KinematicCharacterMotor motor = target as KinematicCharacterMotor;
+            if (motor)
+            {
+                var trans = motor.transform;
+                var transUp = trans.up;
+                Vector3 characterBottom = trans.position +
+                                          (motor.Capsule.center + (-Vector3.up * (motor.Capsule.height * 0.5f)));
+
+                Handles.color = Color.yellow;
+                Handles.CircleHandleCap(
+                    0,
+                    characterBottom + (transUp * motor.MaxStepHeight),
+                    Quaternion.LookRotation(transUp, trans.forward),
+                    motor.Capsule.radius + 0.1f,
+                    EventType.Repaint);
+            }
+        }
+    }
+}
---
+++ KinematicCharacterController/KCCSettings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace KinematicCharacterController
+{
+    [CreateAssetMenu]
+    public class KCCSettings : ScriptableObject
+    {
+        /// <summary>
+        /// Determines if the system simulates automatically. If true, the simulation is done on FixedUpdate
+        /// </summary>
+        public bool AutoSimulation = true;
+
+        /// <summary>
+        /// Should interpolation of characters and PhysicsMovers be handled
+        /// </summary>
+        public bool Interpolate = true;
+
+        /// <summary>
+        /// Initial capacity of the system's list of Motors (will resize automatically if needed, but setting a high
+        /// initial capacity can help preventing GC allocs)
+        /// </summary>
+        public int MotorsListInitialCapacity = 100;
+
+        /// <summary>
+        /// Initial capacity of the system's list of Movers (will resize automatically if needed, but setting a high
+        /// initial capacity can help preventing GC allocs)
+        /// </summary>
+        public int MoversListInitialCapacity = 100;
+    }
+}
