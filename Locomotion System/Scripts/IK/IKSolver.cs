b496072b57f2c9764b45d6d10c514ff9f6a55bb1
林鑫 <linxin025@gmail.com>
2025-05-03 21:33:05 +0800

    Format code

--- Locomotion System/Scripts/IK/IKSolver.cs
+++ Locomotion System/Scripts/IK/IKSolver.cs
@@ -3,13 +3,12 @@ Copyright (c) 2008, Rune Skovbo Johansen & Unity Technologies ApS
 
 See the document "TERMS OF USE" included in the project folder for licencing details.
 */
-using UnityEngine;
-using System.Collections;
 
-public abstract class IKSolver {
-	
-	public float positionAccuracy = 0.001f;
-	
-	public abstract void Solve(Transform[] bones, Vector3 target);
-	
+using UnityEngine;
+
+public abstract class IKSolver
+{
+    public float positionAccuracy = 0.001f;
+
+    public abstract void Solve(Transform[] bones, Vector3 target);
 }
